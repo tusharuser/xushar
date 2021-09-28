8a6c326d9689d2aa36b0f0bcbebb968a7ac14dd2
tusharuser <71004019+tusharuser@users.noreply.github.com>
2021-09-28 13:38:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from os import getenv
 
 
 if os.path.exists("local.env"):
-API_ID_KEY = Config.API_ID
-API_HASH_KEY = Config.API_HASH
+API_ID_KEY = Config.API_ID_KEY
+API_HASH_KEY = Config.API_HASH_KEY
 STRING_SESSION = Config.STRING_SESSION
 MONGO_DB_URL = Config.MONGO_DB_URL
 SIBYL = Config.SIBYL
